915ad1d1bad8e6c3153e1ba035bd2adf5462105e
Karlos Espinoza <karlos.espinoza@academicos.udg.mx>
2024-03-06 13:05:25 -0600

    match

---
+++ python/semana_4/4_match/match.py
@@ -0,0 +1,21 @@
+nombre = input("Ingresa tu nombre: ")
+
+nombre = nombre.capitalize()
+
+match nombre:
+    case "Harry":
+        print("Potter")
+    case "Harmione":
+        print("Gryffindore")
+    case "Ron":
+        print("Gryffindore")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Quien???")
+
+
+
+
+print("Alfedo camate pofavo")
+
